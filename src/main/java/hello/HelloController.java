0aaf263aa22afd639b47aa5994f61bef3fb66b9d
Niro Liyanage <niro.liyanage@touchcorp.com>
2018-01-29 16:14:27 +1100

    Revert "trying metatdata again"
    This reverts commit 37dafb6d11bbc93834e832a04ccd89a72f7aae4a.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -11,4 +11,12 @@ public class HelloController {
         return "Hello World";
     }
 
+    @RequestMapping("/myhelloworld")
+    public String myhelloworld() {
+    final Properties properties = new Properties();
+	properties.load(this.getClass().getResourceAsStream("helloworld.properties"));
+	String versionName = properties.getProperty("version");
+	String artifactID = properties.getProperty("artifactId");
+	return versionName+artifactID;
+	}
 }
