37dafb6d11bbc93834e832a04ccd89a72f7aae4a
Niro Liyanage <niro.liyanage@touchcorp.com>
2018-01-29 16:12:10 +1100

    trying metatdata again

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -31,3 +31,50 @@ public class Application {
     }
 
 }
+
+@EnableConfigurationProperties({MyProperties.class})
+public class ApplicationConfiguration {
+ 
+    @Autowired
+    private MyProperties myProperties;
+ 
+    public MyProperties getMyProperties() {
+        return myProperties;
+    }
+ 
+    public void setMyProperties(MyProperties myProperties) {
+        this.myProperties = myProperties;
+    }
+}
+
+
+@ConfigurationProperties(prefix = "my")
+public class MyProperties {
+ 
+    /**
+     * String property used to configure my app.
+     */
+    private String property;
+ 
+    /**
+     * Configuration for file operations.
+     */
+    private StandardCopyOption copyOption;
+ 
+    public String getProperty() {
+        return property;
+    }
+ 
+    public void setProperty(String property) {
+        this.property = property;
+    }
+ 
+    public StandardCopyOption getCopyOption() {
+        return copyOption;
+    }
+ 
+    public void setCopyOption(StandardCopyOption copyOption) {
+        this.copyOption = copyOption;
+    }
+    
+}
